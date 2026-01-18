9018f3df6b9f780934e025753c60122a46631eb1
SS1GGzxc <sterletivakiril@gmail.com>
2026-01-18 13:05:27 +0400

    fix error screen

--- lib/core/presentation/widgets/error_message_widget.dart
+++ lib/core/presentation/widgets/error_message_widget.dart
@@ -11,77 +11,90 @@ final class ErrorMessageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const Icon(Icons.arrow_back, size: 24, color: Color(0xff000080)),
-          const SizedBox(width: 16),
-          Text(
-            AppLocalizations.of(context)!.goBack,
-            style: TextStyle(
-                fontFamily: "Onder",
-                decoration: TextDecoration.none,
-                fontWeight: FontWeight.w400,
-                fontSize: 40,
-                color: Color(0xFF4069D3)),
-          ),
-          const SizedBox(height: 24),
-          Text(
-            AppLocalizations.of(context)!.somethingWentWrong,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontFamily: "Onder",
-                decoration: TextDecoration.none,
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-                color: Color(0xFF000080)),
-          ),
-          const SizedBox(height: 24),
-          Text(
-            AppLocalizations.of(context)!.serverMessage,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              fontFamily: "SF Pro Display",
-              fontWeight: FontWeight.w400,
-              color: Color(0xff000080),
-            ),
-          ),
-          const SizedBox(height: 32),
-          GestureDetector(
-            onTap: () => context.pop(),
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(16)),
-                  color: Color(0xffF8F8F8),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 16,
-                      color: Color(0x0f000000),
-                      offset: Offset(0, 2),
-                    )
-                  ]),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () => context.pop(),
               child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   const Icon(Icons.arrow_back,
-                      size: 24, color: Color(0xff000080)),
-                  SizedBox(width: 16),
+                      size: 24, color: const Color(0xff000080)),
+                  const SizedBox(width: 16),
                   Text(
                     AppLocalizations.of(context)!.goBack,
                     style: TextStyle(
-                        fontSize: 18,
-                        fontFamily: "SF Pro Display",
+                        fontFamily: "Onder",
+                        decoration: TextDecoration.none,
                         fontWeight: FontWeight.w400,
-                        color: Color(0xff000080)),
+                        fontSize: 40,
+                        color: const Color(0xFF4069D3)),
                   ),
                 ],
               ),
             ),
-          )
-        ],
+            const SizedBox(height: 24),
+            Text(
+              AppLocalizations.of(context)!.somethingWentWrong,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontFamily: "Onder",
+                  decoration: TextDecoration.none,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: const Color(0xFF000080)),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              AppLocalizations.of(context)!.serverMessage,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontFamily: "SF Pro Display",
+                fontWeight: FontWeight.w400,
+                color: const Color(0xff000080),
+              ),
+            ),
+            const SizedBox(height: 32),
+            GestureDetector(
+              onTap: () => context.pop(),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    color: const Color(0xffF8F8F8),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 16,
+                        color: const Color(0x0f000000),
+                        offset: Offset(0, 2),
+                      )
+                    ]),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.arrow_back,
+                        size: 24, color: const Color(0xff000080)),
+                    SizedBox(width: 16),
+                    Text(
+                      AppLocalizations.of(context)!.goBack,
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: "SF Pro Display",
+                          fontWeight: FontWeight.w400,
+                          color: const Color(0xff000080)),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
